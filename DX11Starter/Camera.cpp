67aacb36aecb8bdb7e50454e1b75ad5af4acf88e
Yun Jiang (RIT Student) <yj9812@ad.rit.edu>
2019-02-06 16:50:14 -0500

    v0206

--- DX11Starter/Camera.cpp
+++ DX11Starter/Camera.cpp
@@ -1,15 +1,50 @@
 #include "Camera.h"
 
 Camera::Camera() {
-
+	cameraDirection = XMVectorSet(0, 0, 1, 0);
+	cameraPosition = XMVectorSet(0, 0, -5, 0);
+	cameraRotationX = 0.0f;
+	cameraRotationY = 0.0f;
 }
 
-Camera::Camera() {
+Camera::~Camera() {
 
 }
 
 void Camera::Update() {
+	XMVECTOR up = XMVectorSet(0, 1, 0, 0);
+	if (GetAsyncKeyState('W') & 0x8000) {
+
+		cameraPosition += cameraDirection * XMVectorReplicate(1.0f); // XMVectorReplicate(1.0f) Returns XMVector(1,1,1,1)
+	}
+
+	if (GetAsyncKeyState('S') & 0x8000) {
+
+		cameraPosition -= cameraDirection * XMVectorReplicate(1.0f);
+	}
+
+	if (GetAsyncKeyState('A') & 0x8000) {
+
+		
+	}
+
+	if (GetAsyncKeyState('D') & 0x8000) {
+
+	}
+
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+
+	}
+
+	if (GetAsyncKeyState('X') & 0x8000) {
+
+	}
+
 	//X and Y Rotation
-	DirectX::XMVector3Rotate(cameraDirection, DirectX::XMQuaternionRotationRollPitchYaw(cameraRotationX, cameraRotationY, 0.0f));
+	XMVector3Rotate(cameraDirection, XMQuaternionRotationRollPitchYaw(cameraRotationX, cameraRotationY, 0.0f));
+	//Store to view matrix
+	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(XMMatrixLookToLH(cameraPosition, cameraDirection, up)));
+
+
 
 }
--- DX11Starter/Camera.h
+++ DX11Starter/Camera.h
@@ -1,22 +1,24 @@
 #pragma once
 #include <DirectXMath.h>
-
+#include <Windows.h>
+using namespace DirectX;
 class Camera{
 public:
 	Camera();
-	Camera();
+	~Camera();
 
-	DirectX::XMFLOAT4X4 GetViewMatrix();
-	DirectX::XMFLOAT4X4 GetProjectionMatrix();
+	XMFLOAT4X4 GetViewMatrix();
+	XMFLOAT4X4 GetProjectionMatrix();
 
 	void Update();
 	
 private:
 
-	DirectX::XMFLOAT4X4 viewMatrix;
-	DirectX::XMFLOAT4X4 projectionMatrix;
-	DirectX::XMFLOAT3 cameraPosition;
-	DirectX::XMVECTOR cameraDirection;
+	XMFLOAT4X4 viewMatrix;
+	XMFLOAT4X4 projectionMatrix;
+	XMVECTOR cameraPosition;
+	XMVECTOR cameraDirection;
+	
 
 	float cameraRotationX;
 	float cameraRotationY;
